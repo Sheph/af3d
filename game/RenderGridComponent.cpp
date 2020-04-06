85050a09ffd0279c2ff23516fd44b7c79b07a4f1
Stanislav Vorobiov <sheffmail@mail.ru>
2020-04-06 19:24:32 +0300

    game: grid...

---
+++ game/RenderGridComponent.cpp
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2020, Stanislav Vorobiov
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "RenderGridComponent.h"
+
+namespace af3d
+{
+    ACLASS_DEFINE_BEGIN(RenderGridComponent, RenderComponent)
+    ACLASS_DEFINE_END(RenderGridComponent)
+
+    RenderGridComponent::RenderGridComponent()
+    : RenderComponent(AClass_RenderGridComponent, true)
+    {
+    }
+
+    const AClass& RenderGridComponent::staticKlass()
+    {
+        return AClass_RenderGridComponent;
+    }
+
+    AObjectPtr RenderGridComponent::create(const APropertyValueMap& propVals)
+    {
+        auto obj = std::make_shared<RenderGridComponent>();
+        obj->propertiesSet(propVals);
+        return obj;
+    }
+
+    void RenderGridComponent::update(float dt)
+    {
+    }
+
+    void RenderGridComponent::render(RenderList& rl, void* const* parts, size_t numParts)
+    {
+    }
+
+    std::pair<AObjectPtr, float> RenderGridComponent::testRay(const Frustum& frustum, const Ray& ray, void* part)
+    {
+        auto res = ray.testPlane(plane_);
+        if (res.first) {
+            return std::make_pair(sharedThis(), res.second);
+        } else {
+            return std::make_pair(AObjectPtr(), 0.0f);
+        }
+    }
+
+    void RenderGridComponent::debugDraw()
+    {
+    }
+
+    void RenderGridComponent::onRegister()
+    {
+    }
+
+    void RenderGridComponent::onUnregister()
+    {
+    }
+}
