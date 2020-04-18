3b54060556bb89285a7e4949deb14164ca1caa8e
Stanislav Vorobiov <sheffmail@mail.ru>
2020-04-18 22:28:39 +0300

    game: enable luajit

--- cmake_i386_release.sh
+++ cmake_i386_release.sh
@@ -4,4 +4,5 @@ cd ../${DIR_NAME}-release
 cmake -G "Eclipse CDT4 - Unix Makefiles" \
 -DCMAKE_ECLIPSE_VERSION=4.14 \
 -DCMAKE_BUILD_TYPE="Release" \
+-DUSE_LUAJIT=1 -DUSE_LUAJIT_VALGRIND=0 \
 ../${DIR_NAME}
