2a53d6bde4a3d2952f56ca0e25e26fa59a7e2be7
Stanislav Vorobiov <sheffmail@mail.ru>
2020-05-19 11:31:14 +0300

    game: use ccache/gold

--- cmake_i386_debug.sh
+++ cmake_i386_debug.sh
@@ -5,4 +5,6 @@ cmake -G "Eclipse CDT4 - Unix Makefiles" \
 -DCMAKE_ECLIPSE_VERSION=4.14 \
 -DCMAKE_BUILD_TYPE="Debug" \
 -DUSE_LUAJIT=1 -DUSE_LUAJIT_VALGRIND=0 -DFORCE_OPTIMIZE=1 \
+-DCMAKE_CXX_COMPILER="/usr/lib/ccache/g++" -DCMAKE_C_COMPILER="/usr/lib/ccache/gcc" \
+-DCMAKE_LINKER="/usr/bin/ccache" \
 ../${DIR_NAME}
